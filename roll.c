12bb991142f265d5b973e655958b7d9b54e40e96
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-08-25 14:43:33 +0530

    added advantage and disadvantage flags

--- roll.c
+++ roll.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
 // color list thanks to RabaDabaDoba on github
 // https://gist.github.com/RabaDabaDoba/145049536f815903c79944599c6f952a
@@ -19,15 +20,49 @@ int main(int argc, char *argv[]) {
     int n = 1;
     int a = 0;
 
+    bool adv = false;
+    bool dadv = false;
+
     char *critColor = RED;
     char *failColor = CYN;
 
     if (argc > 1)
         for (int i = 1; i < argc; i++) {
             if (argv[i][0] == '-') {
-                if (argv[i][1] == 'd') d = atoi(argv[++i]);
-                if (argv[i][1] == 'n') n = atoi(argv[++i]);
-                if (argv[i][1] == 'a') a = atoi(argv[++i]);
+                switch (argv[i][1]) {
+                case 'd':
+                    d = atoi(argv[++i]);
+                    break;
+                
+                case 'n':
+                    n = atoi(argv[++i]);
+                    break;
+                
+                case 'a':
+                    a = atoi(argv[++i]);
+                    break;
+
+                case 'A':
+                    adv = true;
+                    break;
+
+                case 'D':
+                    dadv = true;
+                    break;
+                
+                case 'v':
+                    printf("version 0.1.0");
+                    return 0;
+                    break;
+
+                case 'h':
+                    printf("help coming soon :)");
+                    break;
+                
+                default:
+                    printf("help coming soon :)");
+                    break;
+                }
             } else {
                 if (argv[1][0] == 'd')
                     sscanf(argv[1], "d%d+%d", &d, &a);
@@ -38,23 +73,42 @@ int main(int argc, char *argv[]) {
     
     srand(clock());
 
-    int ans = a;
-    for (int i = 0; i < n; i++) {
-        int roll = rand() % d + 1;
-        if (roll == d) printf("\a");
-        printf(
-            "%s%s%d%s",
-            i == 0 ? "" : " + ",
-            roll == d ? critColor : roll == 1 ? failColor : "" ,
-            roll,
-            WHT
-        );
-        ans += roll;
-    }
+    if (adv && !dadv) {
+        if (n != 1) printf("only 1 dice can be rolled with adavntage at a time");
+        else {
+            int roll1 = rand() % d + 1;
+            int roll2 = rand() % d + 1;
+            int ans = a + roll1 > roll2 ? roll1 : roll2;
+            printf("max(%d, %d)", roll1, roll2);
+            if (a > 0) printf(" + %d", a);
+            printf(" = %d\n", ans);
+        }
+    } else if (dadv && !adv) {
+        int roll1 = rand() % d + 1;
+        int roll2 = rand() % d + 1;
+        int ans = a + (roll1 < roll2 ? roll1 : roll2);
+        printf("min(%d, %d)", roll1, roll2);
+        if (a > 0) printf(" + %d", a);
+        printf(" = %d\n", ans);
+    } else {
+        int ans = a;
+        for (int i = 0; i < n; i++) {
+            int roll = rand() % d + 1;
+            if (roll == d) printf("\a");
+            printf(
+                "%s%s%d%s",
+                i == 0 ? "" : " + ",
+                roll == d ? critColor : roll == 1 ? failColor : "" ,
+                roll,
+                WHT
+            );
+            ans += roll;
+        }
 
-    if (a > 0) printf(" + %d = %d\n", a, ans);
-    else if (n == 1) printf("\n");
-    else if (n == 0) printf("0\n");
-    else printf(" = %d\n", ans);
+        if (a > 0) printf(" + %d = %d\n", a, ans);
+        else if (n == 1) printf("\n");
+        else if (n == 0) printf("0\n");
+        else printf(" = %d\n", ans);
+    }
     return 0;
 }
